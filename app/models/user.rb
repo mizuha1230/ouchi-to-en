b65ead67788ba6b471e41fcf29bae7da2b1a884d
mizuha1230 <m63103284@gmail.com>
2021-05-19 20:56:56 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*\d)[a-z\d]{6}+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 end
