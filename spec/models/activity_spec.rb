24d90187d2f907563eebd4ba36b9ca369b34ad14
mizuha1230 <m63103284@gmail.com>
2021-05-22 18:57:22 +0900

    Activityモデルの単体テスト

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @activity = FactoryBot.build(:activity)
+  end
+
+  describe '活動内容の保存' do
+    context '活動内容が投稿できる時' do
+      it '画像・今日の様子・連絡事項があれば投稿できる' do
+        expect(@activity).to be_valid
+      end
+      it '今日の様子が記入されていれば投稿できる' do
+        @activity.image = ''
+        @activity.contact = ''
+        expect(@activity).to be_valid
+      end
+    end
+    context '活動内容が投稿できない時' do
+      it '今日の様子が記入されていなければ投稿できない' do
+        @activity.activity_content = ''
+        @activity.valid?
+        expect(@activity.errors.full_messages).to include("今日の様子を入力してください")
+      end
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @activity.user = nil 
+        @activity.valid?
+        expect(@activity.errors.full_messages).to include("Userを入力してください")
+      end
+    end
+  end
 end
