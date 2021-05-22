e800e4cf7178f524ec36c7f460846ba4e1c8c283
mizuha1230 <m63103284@gmail.com>
2021-05-22 11:03:55 +0900

    activitiesテーブルの作成

--- db/migrate/20210522015624_create_activities.rb
+++ db/migrate/20210522015624_create_activities.rb
@@ -1,7 +1,9 @@
 class CreateActivities < ActiveRecord::Migration[6.0]
   def change
     create_table :activities do |t|
-
+      t.text :activity_content, null: false
+      t.string :contact
+      t.references :user, foreign_key: true 
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_112830) do
+ActiveRecord::Schema.define(version: 2021_05_22_015624) do
+
+  create_table "activities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "activity_content", null: false
+    t.string "contact"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_05_18_112830) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "users"
 end
