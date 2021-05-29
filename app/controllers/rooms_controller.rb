7a47a3fc1e5bb9aa12238c13dee1362f94053b33
mizuha1230 <m63103284@gmail.com>
2021-05-29 18:10:06 +0900

    ルーム作成後の遷移先変更

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,7 +7,7 @@ class RoomsController < ApplicationController
   def create
     @room = Room.new(room_params)
     if @room.save
-      redirect_to root_path
+      redirect_to messages_path
     else
       render :new
     end
