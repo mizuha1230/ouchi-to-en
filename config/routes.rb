8d01f3624fe8c1f9ea05fd82c0d63dc4009d4c9c
mizuha1230 <m63103284@gmail.com>
2021-05-29 11:42:34 +0900

    new,createアクションのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :activities, only: [:index, :new, :create, :show]
   resources :messages,  only: :index
+  resources :rooms, only: [:new, :create]
 end
