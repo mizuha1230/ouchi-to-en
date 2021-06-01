9fbd5d8ed7ccc26483fbf5ff46dce6a60ab78f56
mizuha1230 <m63103284@gmail.com>
2021-06-01 22:00:26 +0900

    バリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,16 @@ class Message < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :heat
   belongs_to_active_hash :condition
+
+  with_options presence: true do
+    validates :heat
+    validates :condition
+    validates :content
+  end
+
+  with_options numericality: { other_than: 1, message: '選択してください' } do
+    validates :heat
+    validates :condition
+  end
+  
 end
