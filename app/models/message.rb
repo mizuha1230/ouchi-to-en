bea2855a030f2eee1be43af0fc755e99dad277ae
mizuha1230 <m63103284@gmail.com>
2021-06-03 20:46:53 +0900

    画像ファイルのアソシエーション記述

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :heat
   belongs_to :condition
