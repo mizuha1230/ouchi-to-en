c14115aece9ae4f2f27ca086e67e4180e7c8f485
mizuha1230 <m63103284@gmail.com>
2021-06-23 21:45:20 +0900

    バリデーションの設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,12 @@
 class Event < ApplicationRecord
+
+  default_scope -> { order(start_time: :asc) }
+
+  validate :start_end_check
+
+  def start_end_check
+    if self.start_time.present? && self.end_time_time.present?
+      errors.add(:end_time, "が開始時刻を上回っています。正しく記入してください。") if self.start > self.end_time
+    end
+  end
 end
