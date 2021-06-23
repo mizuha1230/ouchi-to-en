087a1128cf240815c39a1cfb914baaae701fcb06
mizuha1230 <m63103284@gmail.com>
2021-06-23 21:37:56 +0900

    Eventモデルとeventsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_100112) do
+ActiveRecord::Schema.define(version: 2021_06_23_122941) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2021_05_29_100112) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.datetime "start"
+    t.datetime "end_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
---
+++ spec/factories/events.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :event do
+    title { "MyString" }
+    start { "2021-06-23 21:29:41" }
+    end_time { "2021-06-23 21:29:41" }
+  end
+end
