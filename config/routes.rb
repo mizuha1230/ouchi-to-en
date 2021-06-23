3ff9546c325dc245b137650b212bd3e94b2d42d2
mizuha1230 <m63103284@gmail.com>
2021-06-23 21:37:34 +0900

    fullcalendarの導入と表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "activities#index"
   resources :users, only: [:edit, :update]
-  resources :activities, only: [:index, :new, :create, :show]
+  resources :activities
 
   resources :rooms, only: [:index, :new, :create, :destroy] do
     resources :messages, only: [:index, :create]
