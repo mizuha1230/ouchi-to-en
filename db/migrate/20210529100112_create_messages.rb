62f184e9cc8fd8f2eb1b94b3d3cb0db9779dd063
mizuha1230 <m63103284@gmail.com>
2021-07-17 15:03:53 +0900

    カラム名修正

--- db/migrate/20210529100112_create_messages.rb
+++ db/migrate/20210529100112_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
       t.string :content, null: false
-      t.integer :heat, null: false
-      t.integer :condition, null: false
+      t.integer :heat_id, null: false
+      t.integer :condition_id, null: false
       t.references :room, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
