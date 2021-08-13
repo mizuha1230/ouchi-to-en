eca9afdf966b730e3fff4fb7ccbed4197c4680f1
mizuha1230 <m63103284@gmail.com>
2021-08-13 23:32:32 +0900

    重複したコードの見直し・修正（set_activity）

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,8 @@
 class ActivitiesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :show, :edit, :update, :destroy]
   before_action :redirect_root, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_activity, only: [:show, :edit, :update]
+  
   def index
     @activities = Activity.order("created_at DESC")
   end
@@ -19,15 +21,12 @@ class ActivitiesController < ApplicationController
   end
 
   def show
-    @activity = Activity.find(params[:id])
   end
 
   def edit
-    @activity = Activity.find(params[:id])
   end
 
   def update
-    @activity = Activity.find(params[:id])
     if @activity.update(activity_params)
       redirect_to activity_path
     else
@@ -49,5 +48,9 @@ class ActivitiesController < ApplicationController
 
   def redirect_root
     redirect_to root_path unless current_user.id == 1
+  end
+
+  def set_activity
+    @activity = Activity.find(params[:id])
   end
 end
